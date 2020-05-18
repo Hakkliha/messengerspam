ca02767113446731315690afbe7df9b682a89305
Lorenz <khlernits@gmail.com>
2020-05-19 01:14:20 +0300

    import changes

--- messenger.py
+++ messenger.py
@@ -1,15 +1,15 @@
 from time import sleep
 import pyautogui
-import csv
-import random
-import sys
+from csv import reader
+from random import randint 
+from sys import exit
 
 pyautogui.FAILSAFE = True
 
 results = []
 with open("tumbzilla_labels.csv", encoding="utf8") as csvfile:
-    reader = csv.reader(csvfile)  # change contents to floats
-    for row in reader:  # each row is a list
+    file = reader(csvfile)  # change contents to floats
+    for row in file:  # each row is a list
         results += row  # adds row to array
 
 # Select the first application in the taskbar (should be messenger)
@@ -27,23 +27,23 @@ if s is not None:
     pyautogui.moveTo((int(s[0]) + int(s[2]) // 2),
                      (int(s[1]) + int(s[3]) // 2))
 else:
-    sys.exit("The inputfield was not found")
+    exit("The inputfield was not found")
 
 pyautogui.click()  # click the messenger input field
 
 # randomly select how many messages will be sent
-the_range = random.randint(1, 20)
+the_range = randint(1, 20)
 
 # prints how many messages will be sent
 print(f'Number of lines, that will be printed: {the_range}')
 
 for i in range(the_range):
-    pyautogui.write(results[random.randint(0, len(results))],
-                    interval=(random.randint(1, 5) / 100))  # chooses random message to send from array and types it out with random random interval between letters for each message
+    pyautogui.write(results[randint(0, len(results))],
+                    interval=(randint(1, 5) / 100))  # chooses random message to send from array and types it out with random random interval between letters for each message
     pyautogui.press('enter')  # sends message
     print(i + 1)  # shows what line you are on
     # wait for a random period of time to not trigger messenger suspension for botting
-    sleep(random.randint(0, 3))
+    sleep(randint(0, 3))
 
 
 print('END')
