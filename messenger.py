8d2f3bd090526d4aced1c8cceca9047c6ed38558
Lorenz <khlernits@gmail.com>
2020-05-19 01:09:14 +0300

    ready to try

--- messenger.py
+++ messenger.py
@@ -47,24 +47,3 @@ for i in range(the_range):
 
 
 print('END')
-
-
-'''
-
-# See on mõeldud teise projekti jaoks
-
-def on_press(key):
-    if key == keyboard.Key.esc:
-        return False  # stop listener
-    try:
-        k = key.char  # single-char keys
-    except:
-        k = key.name  # other keys
-    if k in ['1', '2', 'left', 'right']:  # keys of interest
-        # self.keys.append(k)  # store it in global-like variable
-        print('Key pressed: ' + k)
-        return False  # stop listener; remove this if want more keys
-
-listener = keyboard.Listener(on_press=on_press)
-listener.start()  # start to listen on a separate thread
-listener.join()  # remove if main thread is polling self.keys'''
